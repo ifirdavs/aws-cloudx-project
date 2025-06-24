cbf56b3b6afa12a9cd5c05d07404d3f9be9db91b
ifirdavs <fismoilov20@gmail.com>
2025-06-24 19:07:36 +0500

    edit install.sh to add line to run on port 80

--- install.sh
+++ install.sh
@@ -10,4 +10,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 
-uvicorn main:app --host 0.0.0.0 --port 8000
+# uvicorn main:app --host 0.0.0.0 --port 8000
+
+# To run on port 80
+sudo bash -c "source .venv/bin/activate && nohup uvicorn main:app --host 0.0.0.0 --port 80 > uvicorn.log 2>&1 &"
